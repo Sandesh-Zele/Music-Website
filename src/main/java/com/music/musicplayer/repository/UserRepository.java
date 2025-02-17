369f17b67eea2dd5b3071ca23ee160b098afad39
Sandesh <sandeshzele007@gmail.com>
2025-02-17 11:48:26 +0530

    Initial commit - Added music player project

---
+++ src/main/java/com/music/musicplayer/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.music.musicplayer.repository;
+
+import com.music.musicplayer.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+}
+
